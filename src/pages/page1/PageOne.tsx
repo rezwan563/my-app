dadf05bfe954048a99699778838c1592a7877aa6
Rezwan <t.rewan17@gmail.com>
2024-04-24 00:00:37 +0600

    added query as useEffect dependency

--- src/pages/page1/PageOne.tsx
+++ src/pages/page1/PageOne.tsx
@@ -9,30 +9,17 @@ interface People {
 const PageOne = () => {
   const [data, setData] = useState<Array<People>>([]);
   const [searchParams, setSearchParams] = useSearchParams();
-/*
-append
-delete
-set
-entries
-get
-getAll
-has
-key
-size
-sort
+  const query = searchParams.get('data');
+  
+  console.log(query)
 
-
-*/
-  console.log('searchparam\n', searchParams)
-
-  searchParams.forEach((v, k) => console.log(k +':'+ v))
-
+ 
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_PUBLIC_API}/page-one`)
+    fetch(`${import.meta.env.VITE_PUBLIC_API}/page-one?data=${query}`)
       .then((res) => res.json())
       .then((result) => setData(result))
       .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  }, [query]);
   if (data.length > 0) console.log(data);
   return (
     <div>
@@ -40,14 +27,22 @@ sort
       {data?.map((i, index) => (
         <ul key={index} className="mt-2 text-lg">
           <li>
-            {i.name} {i.gender} {i.age} {i.location}
+            {i.name} {i.age}
           </li>
         </ul>
       ))}
-      <button onClick={() => setSearchParams({data: 'cat'})} className="p-2 mr-2 bg-blue-600 text-white  outline-2 outline-blue-50 hover:bg-blue-700">
+      <button
+        onClick={() => setSearchParams({ data: "cat" })}
+        className="p-2 mr-2 bg-blue-600 text-white  outline-2 outline-blue-50 hover:bg-blue-700"
+      >
         Load Cat Data
       </button>
-      <button className="bg-black text-white p-2" onClick={() => setSearchParams({})}>Reset Filter</button>
+      <button
+        className="bg-black text-white p-2"
+        onClick={() => setSearchParams({})}
+      >
+        Reset Filter
+      </button>
     </div>
   );
 };
