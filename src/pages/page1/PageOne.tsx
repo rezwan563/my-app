92def2f06d84ad1fa1aa572d8fcb5360e1cd733f
Rezwan <t.rewan17@gmail.com>
2024-04-25 22:24:30 +0600

    installed tanstack query and changed fetched data

--- src/pages/page1/PageOne.tsx
+++ src/pages/page1/PageOne.tsx
@@ -1,48 +1,43 @@
 import { useEffect, useState } from "react";
+import { useQueryClient } from "react-query";
 import { useSearchParams } from "react-router-dom";
-interface People {
-  name: string;
-  gender: string;
-  age: string;
-  location: string;
-}
-const PageOne = () => {
-  const [data, setData] = useState<Array<People>>([]);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const query = searchParams.get('data');
-  
-  console.log(query)
 
- 
+interface Message {
+  message: string;
+}
+
+const PageOne = () => {
+  const [data, setData] = useState<Message>({ message: "" });
+
+  const queryClient = useQueryClient();
+
+/**
+ * self-note: if we use useEffect to fetch data
+ * 
+ * for split second data.message is not found and 'no data' is shown
+ * 
+ * ideal behavior would be to store data somewhere(storage/ cache) 
+ * 
+ * if no new data available data should be loaded from storage/ cache
+ * 
+ * if new data available it will fetch the new data but this time it will display old data(old data which is stale)
+ * 
+ * after reloading the page it will display the new fetched data
+ */
+
   useEffect(() => {
-    fetch(`${import.meta.env.VITE_PUBLIC_API}/page-one?data=${query}`)
+    fetch(`${import.meta.env.VITE_PUBLIC_API}/api/sayhi`)
       .then((res) => res.json())
-      .then((result) => setData(result))
+      .then((result) => {
+        setData(result);
+        console.log(result);
+      })
       .catch((error) => console.error("Error fetching data:", error));
-  }, [query]);
-  if (data.length > 0) console.log(data);
+  }, []);
   return (
     <div>
       <p className="text-7xl">Page One</p>
-      {data?.map((i, index) => (
-        <ul key={index} className="mt-2 text-lg">
-          <li>
-            {i.name} {i.age}
-          </li>
-        </ul>
-      ))}
-      <button
-        onClick={() => setSearchParams({ data: "cat" })}
-        className="p-2 mr-2 bg-blue-600 text-white  outline-2 outline-blue-50 hover:bg-blue-700"
-      >
-        Load Cat Data
-      </button>
-      <button
-        className="bg-black text-white p-2"
-        onClick={() => setSearchParams({})}
-      >
-        Reset Filter
-      </button>
+      <p className="text-5xl">{data.message ? data.message : "No data"}</p>
     </div>
   );
 };
