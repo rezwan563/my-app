fd03fd1050545efa443c5253246f1be52e962dd1
Rezwan <t.rewan17@gmail.com>
2024-04-26 10:02:32 +0600

    added react query to fetch data

--- src/pages/page1/PageOne.tsx
+++ src/pages/page1/PageOne.tsx
@@ -1,43 +1,59 @@
-import { useEffect, useState } from "react";
-import { useQueryClient } from "react-query";
-import { useSearchParams } from "react-router-dom";
-
-interface Message {
-  message: string;
-}
+// import { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+// import { useSearchParams } from "react-router-dom";
 
 const PageOne = () => {
-  const [data, setData] = useState<Message>({ message: "" });
+  // const [data, setData] = useState<Message>({ message: "" });
 
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
 
-/**
- * self-note: if we use useEffect to fetch data
- * 
- * for split second data.message is not found and 'no data' is shown
- * 
- * ideal behavior would be to store data somewhere(storage/ cache) 
- * 
- * if no new data available data should be loaded from storage/ cache
- * 
- * if new data available it will fetch the new data but this time it will display old data(old data which is stale)
- * 
- * after reloading the page it will display the new fetched data
- */
+  /**
+   * self-note: if we use useEffect to fetch data
+   *
+   * for split second data.message is not found and 'no data' is shown
+   *
+   * ideal behavior would be to store data somewhere(storage/ cache)
+   *
+   * if no new data available data should be loaded from storage/ cache
+   *
+   * if new data available it will fetch the new data but this time it will display old data(old data which is stale)
+   *
+   * after reloading the page it will display the new fetched data
+   */
 
-  useEffect(() => {
-    fetch(`${import.meta.env.VITE_PUBLIC_API}/api/sayhi`)
-      .then((res) => res.json())
-      .then((result) => {
-        setData(result);
-        console.log(result);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
+  // useEffect(() => {
+  //   fetch(`${import.meta.env.VITE_PUBLIC_API}/api/sayhi`)
+  //     .then((res) => res.json())
+  //     .then((result) => {
+  //       setData(result);
+  //       console.log(result);
+  //     })
+  //     .catch((error) => console.error("Error fetching data:", error));
+  // }, []);
+
+  const getMessage = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_PUBLIC_API}/api/sayhi`
+      );
+      const result = response.json();
+      return result;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const { data, status } = useQuery(["data"], getMessage);
+  console.log(status);
   return (
     <div>
       <p className="text-7xl">Page One</p>
-      <p className="text-5xl">{data.message ? data.message : "No data"}</p>
+
+      {status === "loading" && <p>Loading...</p>}
+
+      {status === "error" && <p>Error while fetching data</p>}
+
+      {status === "success" && <p>{data.message}</p>}
     </div>
   );
 };
