5bf9d56cd84b399ec3aa5627eae367b1eed4e691
Rezwan <t.rewan17@gmail.com>
2024-04-28 17:31:12 +0600

    Added logout form

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,29 +1,70 @@
+
 const Login = () => {
 
-    const handleLoginForm = (e: React.SyntheticEvent) =>{
-        e.preventDefault()
+    const handleLoginForm = async(e: React.SyntheticEvent) => {
+        e.preventDefault();
         const formData = new FormData(e.target as HTMLFormElement);
-        for(const [key, value] of formData){
-            console.log(`${key} : ${value}`)
+        for (const [key, value] of formData) {
+            console.log(`${key} : ${value}`);
+        }
+        const response = await fetch(`${import.meta.env.VITE_PUBLIC_API}/api/login`, {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json"
+            }
+        })
+        return response.json()
+    };
+
+
+    const handleLogout = async(e: React.SyntheticEvent) =>{
+      e.preventDefault();
+      const formData = new FormData(e.target as HTMLFormElement);
+        for (const [key, value] of formData) {
+            console.log(`${key} : ${value}`);
         }
     }
   return (
     <div>
-      <form onSubmit={handleLoginForm} className="w-fit ml-1 mt-1 p-2 border border-1 border-black">
+      <form
+        onSubmit={handleLoginForm}
+        className="w-fit ml-1 mt-1 p-2 border border-1 border-black"
+      >
         <div className="flex flex-col">
           <label className="flex flex-col">
             Email
-            <input type="email" name="email" aria-activedescendant="enter email" aria-braillelabel="email" />
+            <input type="email" name="email" />
           </label>
           <label className="flex flex-col">
             Password
-            <input type="password" 
-            name="password"
-            aria-activedescendant="enter password"
-            aria-braillelabel="password" />
+            <input type="password" name="password" />
           </label>
         </div>
-        <input type="submit" className="border border-1 border-black p-1 mt-1" value='Login' />
+        <input
+          type="submit"
+          className="border border-1 border-black p-1 mt-1"
+          value="Login"
+        />
+      </form>
+      <form
+        onSubmit={handleLogout}
+        className="w-fit ml-1 mt-1 p-2 border border-1 border-black"
+      >
+        <div className="flex flex-col">
+          <label className="flex flex-col">
+            Email
+            <input type="email" name="email" />
+          </label>
+          <label className="flex flex-col">
+            Password
+            <input type="password" name="password" />
+          </label>
+        </div>
+        <input
+          type="submit"
+          className="border border-1 border-black p-1 mt-1"
+          value="Logout"
+        />
       </form>
     </div>
   );
