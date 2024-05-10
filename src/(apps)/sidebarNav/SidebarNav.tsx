b5938d356f445842ac000b613ef4b8111c3262ea
Rezwan <t.rewan17@gmail.com>
2024-05-10 20:41:37 +0600

    Changed sidebarItems structure

--- src/(apps)/sidebarNav/SidebarNav.tsx
+++ src/(apps)/sidebarNav/SidebarNav.tsx
@@ -1,48 +1,64 @@
-import './SidebarNav.css'
+import "./SidebarNav.css";
 
 const sidebarItems = [
-    {
-      id: 1,
-      label: "Inventory",
-      icon: "dashboard",
-      route: "/inventory",
-    },
-    {
-      id: 2,
-      label: "Products",
-      icon: "box",
-      route: "/inventory/products",
-    },
-    {
-      id: 3,
-      label: "Categories",
-      icon: "folder",
-      route: "/inventory/categories",
-    },
-    {
-      id: 4,
-      label: "Orders",
-      icon: "shopping-cart",
-      route: "/inventory/orders",
-    },
-    {
-      id: 5,
-      label: "Customers",
-      icon: "users",
-      route: "/inventory/customers",
-    },
-    // Add more sidebar items as needed
-  ];
-  
+  {
+    id: 1,
+    label: "Inventory",
+    icon: "dashboard",
+    route: "/inventory",
+    subroute: [
+      {
+        id: 11,
+        label: "Products",
+        icon: "box",
+        route: "/inventory/products",
+      },
+      {
+        id: 12,
+        label: "Categories",
+        icon: "folder",
+        route: "/inventory/categories",
+      },
+      {
+        id: 13,
+        label: "Orders",
+        icon: "shopping-cart",
+        route: "/inventory/orders",
+      },
+      {
+        id: 14,
+        label: "Customers",
+        icon: "users",
+        route: "/inventory/customers",
+      },
+    ],
+  },
+
+  // Add more sidebar items as needed
+];
 
 const SidebarNav = () => {
-    return (
-        <ul>
-            {sidebarItems.map((item) => (
-                <li key={item.id}><a href={item.route}>{item.label}</a></li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul>
+      {sidebarItems.map((item, index) => (
+        <li key={item.id}>
+          <a href={item.route}>{item.label}</a>
+
+          {sidebarItems[index].subroute.length > 1 ? (
+            <ul>
+              {sidebarItems[index].subroute.map((sub) => (
+                <li key={sub.id} className="sub">
+                  <a href={sub.route}>{sub.label}</a>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            ""
+          )}
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default SidebarNav;
